c4dddd7d72f6dcfcc03d430936e3718d6ac7f798
Hakkiilginli <160836300+Hakkiilginli@users.noreply.github.com>
2024-02-22 11:34:36 +0300

    FoodyTekmerWeb
    Asp.Net 6.0 ile geliştirilmiştir

---
+++ FoodyTekmerWebUI-master/FoodyTekmerBussinesLayer/Abstract/ITestimonialService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodyTekmerEntityLayer.Concrete;
+
+namespace FoodyTekmerBussinesLayer.Abstract
+{
+    internal interface ITestimonialService:IGenericService<Testimonial>
+    {
+    }
+}
---
+++ FoodyTekmerWebUI-master/FoodyTekmerWebUI/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using FoodyTekmerDataAccessLayer.Context;
+using FoodyTekmerEntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodyTekmerWebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        FoodyContext context = new FoodyContext();
+        public IActionResult Index()
+        {
+            var values = context.Categories.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category p)
+        {
+            context.Categories.Add(p);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = context.Categories.Find(id);
+            context.Categories.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCategory(int id)
+        {
+            var value = context.Categories.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCategory(Category p)
+        {
+            context.Categories.Update(p);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
